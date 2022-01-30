4bfae6ce3f461f31f9935e4bf7949fd87faee84a
mlinds <45837856+mlinds@users.noreply.github.com>
2022-01-30 16:48:19 +0100

    refactored several functions and wrote new tests

--- cabi/test_cabi.py
+++ cabi/test_cabi.py
@@ -1,19 +1,39 @@
+from operator import ge
 from cabi import cabi
-import pandas as pd 
+import pandas as pd
 import numpy as np
+import geopandas as gpd
+from shapely.geometry import LineString
+import pytest
 
 
 def test_load_stations():
-    station_df = cabi.load_stations()
-    assert station_df.short_name.dtype is np.dtype('int32')
+    station_df = cabi._get_station_dataframe()
+    assert station_df.short_name.dtype is np.dtype("int32")
+    assert type(station_df) is pd.DataFrame
+    station_df = cabi._get_station_dataframe()
 
 
 def test_find_station_geo():
     station = 31000
     print(station)
-    lat,lon = cabi.find_station_geo(station)
+    lat, lon = cabi.find_station_geo(station)
     assert float(lon) == 38.858971
     assert float(lat) == -77.053230
 
+
 def test_get_dist():
-    distance = cabi.get_dist(31000,31003)
+    distance = cabi.get_dist(31000, 31003)
+    assert type(distance) is float
+
+    with pytest.raises(KeyError):
+        cabi.get_dist(50, 31003)
+
+
+def test_make_trip_geometry():
+    geometry = cabi.make_trip_geometry(31000, 31003)
+    assert type(geometry) is LineString
+
+
+def test_find_station_geo():
+    pass
