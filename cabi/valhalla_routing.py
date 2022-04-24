a641e28c49a51fff5df9bd8609e2dd5d8454f2d5
mlinds <max.lindsay95@gmail.com>
2022-04-24 14:23:31 +0200

    using python bindings from gisops

--- cabi/valhalla_routing.py
+++ cabi/valhalla_routing.py
@@ -1,51 +1,35 @@
 # %%
-import requests
 import geopandas as gpd
-from shapely.geometry import shape
 import pandas as pd
 from numpy import array_split
 from itertools import islice
 from sqlalchemy import create_engine
 from tqdm import tqdm
-import json
 from shapely.geometry import LineString
 import numpy as np
 from valhalla import Actor, get_config, get_help
+from valhalla.utils import decode_polyline
 
 engine = create_engine("postgresql://admin:maxpass@127.0.0.1:5432/pgrouting_data")
 #%%
 
-#decode an encoded string
-def decode(encoded):
-    #six degrees of precision in valhalla
-  inv = 1.0 / 1e6;
-  decoded = []
-  previous = [0,0]
-  i = 0
-  #for each byte
-  while i < len(encoded):
-    #for each coord (lat, lon)
-    ll = [0,0]
-    for j in [0, 1]:
-      shift = 0
-      byte = 0x20
-      #keep decoding bytes until you have this coord
-      while byte >= 0x20:
-        byte = ord(encoded[i]) - 63
-        i += 1
-        ll[j] |= (byte & 0x1f) << shift
-        shift += 5
-      #get the final value adding the previous offset and remember it for the next
-      ll[j] = previous[j] + (~(ll[j] >> 1) if ll[j] & 1 else (ll[j] >> 1))
-      previous[j] = ll[j]
-    #scale by the precision and chop off long coords also flip the positions so
-    #its the far more standard lon,lat instead of lat,lon
-    decoded.append([float('%.6f' % (ll[1] * inv)), float('%.6f' % (ll[0] * inv))])
-  #hand back the list of coordinates
-  return decoded
+config = get_config('../data/osm_data/valhalla_tiles.tar',verbose=False)
+actor = Actor(config)
 
-# tool that queries the running instance of OSMR
+# tool that queries the running instance of valhalla
 def get_route(startx, starty, endx, endy):
+    """Find a route in DC using valhalla
+
+    Args:
+        startx (float): starting longitude
+        starty (float): starting latitude
+        endx (float): ending longitude
+        endy (float): ending latirude
+
+    Returns:
+        LineString: Shapeley linestring of the assumed route
+    """    
+    # set up the routing parameters
     requeststring = {
         "locations": [{"lat":starty,"lon":startx},{"lat":endy,"lon":endx}],
         "costing":"bicycle",
@@ -59,45 +43,46 @@ def get_route(startx, starty, endx, endy):
         },
         "directions_type":"none"
     }
-
-    r = requests.get(f"http://127.0.0.1:8002/route?json=" + json.dumps(requeststring))
-    if r.status_code == 400:
+    # attempt routing and give a NaN if there is an issue
+    try: 
+        response = actor.route(requeststring)
+        polyline_enc = response['trip']['legs'][0]['shape']
+        polyline_dec = decode_polyline(polyline_enc)
+        return LineString(polyline_dec)
+    except RuntimeError:
+        print(f'{(startx, starty, endx, endy)} couldnt be routed')
         return np.NaN
-    polyline_enc = r.json()['trip']['legs'][0]['shape']
-    polyline_dec = decode(polyline_enc)
 
-    return LineString(polyline_dec)
+def main():
+    # load the dataframe of route popularity
+    pairs = pd.read_csv("../data/processed/connections_csv.csv")
+    # load the stations
+    stations = pd.read_csv("../data/processed/stationLookup.csv", index_col="short_name")
+    lookup_series = stations.loc[:,['lat','lon']]
 
-# %%
-# load the dataframe of route popularity
-pairs = pd.read_csv("../data/processed/connections_csv.csv")
-# load the stations
-stations = pd.read_csv("../data/processed/stationLookup.csv", index_col="short_name")
-lookup_series = pd.Series(
-    list(stations.loc[:, ["lon", "lat"]].itertuples(index=False, name=None)),
-    index=stations.index,
-    name="latlong",
-)
+    # %%
 
-# combine the stations and the route popularity, and make them into tuples of the latlongs
-mergedf = (
-    pairs.merge(
-        lookup_series, how="left", left_on="st", right_index=True, validate="m:1"
-    )
-    .rename(columns={"latlong": "start_latlong"})
-    .merge(lookup_series, how="left", left_on="en", right_index=True, validate="m:1")
-    .rename(columns={"latlong": "end_latlong"})
-)
-
-# %%
-# to limit the amount of memory, we do the database loading in chunks
-for chunk in tqdm(array_split(mergedf, 100)):
-    geomlist = [
-        get_route(*startlatlong, *endlatlong)
-        for startlatlong, endlatlong in zip(chunk.start_latlong, chunk.end_latlong)
-    ]
-    gpd.GeoDataFrame(chunk).assign(geom=geomlist).dropna().set_geometry('geom',crs="EPSG:4326").to_postgis(
-        "station_routes", con=engine, if_exists="append", index=False
+    # combine the stations and the route popularity, and make them into tuples of the latlongs
+    mergedf = (
+        pairs.merge(
+            lookup_series, how="left", left_on="st", right_index=True, validate="m:1",
+        )
+        .rename(columns={"lon": "start_long","lat": "start_lat"})
+        .merge(lookup_series, how="left", left_on="en", right_index=True, validate="m:1")
+        .rename(columns={"lon": "end_long","lat": "end_lat"})
     )
 
-# %%
+    # %%
+    # to limit the amount of memory, we do the database loading in chunks
+    for chunk in tqdm(array_split(mergedf, 100)):
+        geomlist = [
+            get_route(startx, starty, endx, endy)
+            for starty,startx, endy,endx in chunk.iloc[:,3:].values
+        ]
+        # load the chunk into a gdf and write to the postgis database
+        gpd.GeoDataFrame(chunk).assign(geom=geomlist).dropna().set_geometry('geom',crs="EPSG:4326").to_postgis(
+            "station_routes", con=engine, if_exists="append", index=False
+        )
+
+if __name__ == '__main__':
+    main()
