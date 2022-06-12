b9ec47f50f881f3f402f9e0868c3c22acfbbaed1
mlinds <max.lindsay95@gmail.com>
2022-06-12 16:01:24 +0200

    runs in pgadmin

--- cabi/queries/topogeo_trigger.sql
+++ cabi/queries/topogeo_trigger.sql
@@ -2,13 +2,27 @@ create or replace function trig_geo2topogeo()
 
 returns trigger as $$
 begin
-update routes_topogeo 
-    set topogeom = toTopoGeom(NEW.geom,'cabi_topo',1,0.1)
+
+    IF (TG_OP = 'DELETE') THEN
+            delete from routes_topogeo_test
+            where old.st = routes_topogeo.st and old.en = routes_topogeo.en;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            update routes_topogeo_test
+                set topogeom = toTopoGeom(NEW.geom,'cabi_topo',1,0.1);
+
+        ELSIF (TG_OP = 'INSERT') THEN
+                    
+            insert into routes_topogeo_test(st, en, topogeom)
+            select miss_top.st,
+                miss_top.en,
+                toTopoGeom(miss_top.geom, 'cabi_topo', 9, 0.1);
+        END IF;
 
 end;
+$$
 language plpgsql;
 
 create trigger update_topogeo 
     after insert or update on route_geometry
-    for row 
-    execute procedure 
+    for each row
+    execute procedure trig_geo2topogeo();
