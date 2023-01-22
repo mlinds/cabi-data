cecef20f44330ce82011b18726652ee0a58760f3
mlinds <max.lindsay95@gmail.com>
2023-01-22 12:18:04 +0100

    accidently skipped virginia

--- cabi/process_osm.sh
+++ cabi/process_osm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install osmctools -y
-osmconvert ..data/osm_data/states/district-of-columbia-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599 \
- | osmconvert - ..data/osm_data/states/maryland-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599 \
- | osmconvert - ..data/osm_data/states/district-of-columbia-latest.osm.pbf -b=-77.3724,38.7685,-76.8474,39.599 -o=all_dc_area.pbf
+# sudo apt install osmctools -y
+osmconvert /custom_files/states/district-of-columbia-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599 \ 
+| osmconvert - /custom_files/states/maryland-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599  \
+| osmconvert - /custom_files/states/virginia-latest.osm.pbf -b=-77.3724,38.7685,-76.8474,39.599 -o=all_dc_area.pbf
