979114034a1b3fb5c0290e965607922a81841a4e
mlinds <max.lindsay95@gmail.com>
2023-02-12 16:59:32 +0100

    trigger function is working

--- cabi/queries/topogeo_trigger.sql
+++ cabi/queries/topogeo_trigger.sql
@@ -1,28 +1,27 @@
-create or replace function trig_geo2topogeo()
-
-returns trigger as $$
+create or replace function trig_geo2topogeo() returns trigger as $$
 begin
-
     IF (TG_OP = 'DELETE') THEN
-            delete from routes_topogeo_test
-            where old.st = routes_topogeo.st and old.en = routes_topogeo.en;
+            delete from routes_topogeo
+            where OLD.st = routes_topogeo.st and Old.en = routes_topogeo.en;
         ELSIF (TG_OP = 'UPDATE') THEN
-            update routes_topogeo_test
+            update routes_topogeo
                 set topogeom = toTopoGeom(NEW.geom,'cabi_topo',1,0.1);
 
         ELSIF (TG_OP = 'INSERT') THEN
-                    
-            insert into routes_topogeo_test(st, en, topogeom)
-            select miss_top.st,
-                miss_top.en,
-                toTopoGeom(miss_top.geom, 'cabi_topo', 9, 0.1);
+            insert into routes_topogeo(st, en, topogeom)
+            select NEW.st,
+                NEW.en,
+                toTopoGeom(NEW.geom, 'cabi_topo2', 1, 0.1);
         END IF;
-
+	return NULL;
 end;
-$$
-language plpgsql;
+$$ language plpgsql;
 
-create trigger update_topogeo 
-    after insert or update on route_geometry
-    for each row
-    execute procedure trig_geo2topogeo();
+
+create or replace trigger update_topogeo after
+insert
+or
+update
+or
+delete on route_geometry
+for each row execute procedure trig_geo2topogeo();
