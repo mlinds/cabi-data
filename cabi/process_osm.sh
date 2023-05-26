e56f0653eeed55746044dd19538ee57484c3e1d3
mlinds <max.lindsay95@gmail.com>
2023-05-26 19:44:31 +0200

    clean up osm download script

--- cabi/process_osm.sh
+++ cabi/process_osm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# sudo apt install osmctools -y
-osmconvert /custom_files/states/district-of-columbia-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599 \ 
-| osmconvert - /custom_files/states/maryland-latest.osm.pbf --out-o5m -b=-77.3724,38.7685,-76.8474,39.1599  \
-| osmconvert - /custom_files/states/virginia-latest.osm.pbf -b=-77.3724,38.7685,-76.8474,39.599 -o=all_dc_area.pbf
+# wget http://download.geofabrik.de/north-america/us/virginia-latest.osm.pbf
+# wget http://download.geofabrik.de/north-america/us/maryland-latest.osm.pbf
+# wget http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf
+sudo apt install osmctools -y
+osmconvert ./states/virginia-latest.osm.pbf --out-o5m -b=-77.4,38.7,-76.7,39.2 | osmconvert - ./states/maryland-latest.osm.pbf --out-o5m -b=-77.4,38.7,-76.7,39.2 | osmconvert - ./states/district-of-columbia-latest.osm.pbf -b=-77.4,38.7,-76.7,39.2 -o=all_dc_area.pbf
